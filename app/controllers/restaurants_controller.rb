c27edb2f1b1e7a12d2a92a09faf0d40f50327c62
elia benedetti <eliabenedetti04@gmail.com>
2023-11-15 15:49:43 +0100

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,31 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :reviews)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+rest_name_array = ['la goulue', "l'esplanade", 'chez francis', 'restaurante botin', 'dal bolognese',
+                          'bardo st. james', 'tausend', "l'aperousse"]
+rest_addres_array = %w[paris rome milan london madrid berlin geneva brussels amsterdam bordeaux barcelona]
+rest_categ_array = %w[chinese japanese italian french belgian]
+
+reviews_content_array = ['not bad', 'bad', 'terrible', 'good', 'tasty', 'nice staff', 'fine', 'delicious']
+reviews_rating_array = [0..5]
+restaurant_id_array = [1..60]
+
+15.times do
+  Restaurant.create(name: rest_name_array.sample, address: rest_addres_array.sample, category: rest_categ_array.sample)
+end
+
+150.times do
+  review = Review.new(content: reviews_content_array.sample, rating: reviews_rating_array.sample)
+  review.restaurant_id = restaurant_id_array.sample
+  review.save
+end
